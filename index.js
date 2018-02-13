e0e273608a8a727549edc64b7206da2e0632ccc8
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-02-13 14:43:21 +0300

    hop

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(9000)
-console.log(`App serve at: http://localhost:9000/`)
+const PORT = process.env.PORT || 9000
+
+app.set('port', PORT)  
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
